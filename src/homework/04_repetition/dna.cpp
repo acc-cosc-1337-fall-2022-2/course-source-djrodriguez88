1ffeb2304e661706557419832353949ae3c4c279
djrodriguez88 <daniel.rodriguez15@g.austincc.edu>
2022-10-14 20:58:17 +0000

    Homework 4 with test cases

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,37 @@
 //add include statements
+#include "dna.h"
 
 //add function(s) code here
+
+  
+
+int factorial(int num)
+{
+     int factProduct = 1;
+
+    while (num > 0)
+    {
+        factProduct = (factProduct)*(num);
+        num--;
+    }
+    return factProduct;
+}
+
+int gcd(int num1, int num2)
+{
+        int num3;
+        while(num1 != num2)
+        {
+            if (num1 < num2)
+            {
+                num3 = num1;
+                num1 = num2;
+                num2 = num3;
+            }
+            if (num1 > num2)
+            {
+                num1 = num1 - num2;
+            }
+        }
+    return num1;
+}
--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,16 +1,51 @@
 //write include statements
-
+#include<iostream>
+#include "dna.h"
 //write using statements
+using std::string;
+using std::cout;
+using std::cin;
 
-
-/*
-Write code that prompts user to enter 1 for Get GC Content, 
-or 2 for Get DNA Complement.  The program will prompt user for a 
-DNA string and call either get gc content or get dna complement
-function and display the result. Program runs as long as 
-user enters a y or Y.
-*/
 int main() 
 {
+	string option;
+	int quit = 0;
+	int gDenom;
+	
+	do
+	{
+		std::cout<<"Please choose between the following options: \n";
+		std::cout<<" 1 - Factorial \n 2 - Greatest Common Divisor \n 3 - Exit  \n";
+		std::cin>>option;
+
+		if(option == "1")
+		{
+			int factProduct = 0;
+			int fNum;
+            std::cout<<"Please enter a number to determine it's factorial.";
+			std::cin>>fNum;
+			factProduct = factorial(fNum);
+			std::cout<<"The factorial product is "<<factProduct<<"\n";
+		}
+       	else if(option == "2")
+		{
+			int inputNum1;
+			int inputNum2;
+            std::cout<<"Please enter two numbers to determine their greatest common denominator.\n";
+			std::cout<<"Enter the 1st number.\n";
+			std::cin>>inputNum1;
+			std::cout<<"Now enter the 2nd number.\n";
+			std::cin>>inputNum2;
+			gDenom = gcd(inputNum1, inputNum2);
+			std::cout<<"The Greatest Common Denominator between "<<inputNum1<<" and "<<inputNum2<<" is "<<gDenom<<"\n";
+		}
+		else if(option == "3")
+		{
+            std::cout<<"Are you sure you want to quit? Enter 1 Exit.";
+			std::cin>>quit;
+		}
+	}
+	while(quit != 1);
+	
 	return 0;
 }
